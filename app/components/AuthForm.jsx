1714a1e4dabf81a1f105b15b2045d54035c8b31d
Manolis <manolis-9@hotmail.com>
2023-12-27 16:04:08 +0200

    Watch list | First commit

---
+++ app/components/AuthForm.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Auth } from "@supabase/auth-ui-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
+
+export default function AuthForm() {
+  const supabase = createClientComponentClient();
+
+  const customTheme = {
+    default: {
+      colors: {
+        brand: "hsl(153 60.0% 53.0%)",
+        brandAccent: "hsl(154 54.8% 45.1%)",
+        brandButtonText: "white",
+        // ..
+      },
+    },
+    dark: {
+      colors: {
+        brandButtonText: "white",
+        defaultButtonBackground: "#2e2e2e",
+        defaultButtonBackgroundHover: "#3e3e3e",
+        //..
+      },
+    },
+    // You can also add more theme variations with different names.
+    evenDarker: {
+      colors: {
+        brandButtonText: "white",
+        defaultButtonBackground: "#1e1e1e",
+        defaultButtonBackgroundHover: "#2e2e2e",
+        //..
+      },
+    },
+  };
+
+  return (
+    <>
+      <Auth
+        supabaseClient={supabase}
+        view="magic_link"
+        showLinks={false}
+        providers={["google"]}
+        redirectTo="http://localhost:3000/auth/callback"
+        appearance={{ theme: customTheme }}
+        theme="dark"
+      />
+    </>
+  );
+}
