18d68862145bd061b9bfa312d4ccd9c4db865ef8
Manolis <manolis-9@hotmail.com>
2023-12-28 13:27:23 +0200

    Landing page style fixes

--- app/page.jsx
+++ app/page.jsx
@@ -7,9 +7,9 @@ export default function Home() {
       <div className="mx-auto py-[8vh] h-[100vh] container px-10">
         <div className="bg-[#f2f2f2] flex h-full rounded-[30px]">
           <div className="hidden w-[50%] md:block bg-[url('/bg-photo.jpg')] bg-cover bg-no-repeat bg-center rounded-l-[30px]"></div>
-          <div className="flex w-full md:w-[50%] p-12 justify-center items-center">
+          <div className="flex w-full md:w-[60%] lg:w-[50%] p-12 justify-center items-center">
             <div className="w-[100%] md:w-[70%]">
-              <h1 className="text-2xl md:text-3xl text-[#152B32] mb-4">
+              <h1 className="font-Gabarito text-2xl md:text-3xl text-[#152B32] mb-4">
                 Welcome to
               </h1>
               <h1 className="font-Yellowtail text-5xl md:text-6xl font-extrabold text-[#152B32] pb-12">
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
       fontFamily: {
         Yellowtail: ["Yellowtail", "serif"],
+        Gabarito: ["Gabarito", "serif"],
       },
     },
   },
