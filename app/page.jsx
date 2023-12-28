4a8f8a66827d46eca0b9b5f7c92fc420e7164910
Manolis <manolis-9@hotmail.com>
2023-12-28 18:27:28 +0200

    Fix white border at img bg

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-[#152B32] text-gray-300 font-sans">
       <div className="mx-auto py-[8vh] h-[100vh] container px-6 md:px-10">
-        <div className="bg-[#f2f2f2] flex h-full rounded-[30px]">
+        <div className="bg-[#f2f2f2] flex h-full rounded-[32px]">
           <div className="hidden w-[50%] md:block bg-[url('/bg-photo.jpg')] bg-cover bg-no-repeat bg-center rounded-l-[30px]"></div>
           <div className="flex w-full md:w-[60%] lg:w-[50%] p-12 justify-center items-center">
             <div className="w-[100%] md:w-[70%]">
